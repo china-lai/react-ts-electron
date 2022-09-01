088e3b5f7dd9985626fa871d6ed82e25b2cbaf3a
Lai <1626282810@qq.com>
2022-09-01 08:22:35 +0800

    1. learn ts

---
+++ main/learn-ts/types-from-types/conditional-types.ts
@@ -0,0 +1,71 @@
+// 条件类型
+
+interface Animal {
+    live(): void
+}
+interface Dog extends Animal {
+    woof(): void
+}
+
+type Example1 = Dog extends Animal ? number : string // number
+type Example2 = RegExp extends Animal ? number : string // string
+
+// 重载使用条件表达式
+
+interface IdLabel {
+    id: number
+}
+interface NameLabel {
+    name: string
+}
+// 三个重载
+function createLabel(id: number): IdLabel;
+function createLabel(name: string): NameLabel;
+function createLabel(nameOrId: string | number): IdLabel | NameLabel
+function createLabel(id: number | string): IdLabel | NameLabel {
+    throw "unimplemented"
+}
+
+// 直接实现一种
+// 目前实现也是报错 不明所以
+// --TODO--
+type NameOrId<T extends number | string> = T extends number ? IdLabel : NameLabel;
+function createLabelConditional<T extends number | string>(idOrName: T): NameOrId<T> {
+    throw "unimplemented";
+}
+
+// 条件类型约束
+type MessageOf1<T extends {message: unknown}> = T["message"]
+type MessageOf2<T> = T extends {message: unknown} ? T["message"]: never;
+interface Email {
+    message: string;
+}
+type EmailMessageContents = MessageOf1<Email> // string
+// type RegExpMessageContents1 = MessageOf1<RegExp> // error
+type RegExpMessageContents2 = MessageOf2<RegExp> // never
+
+// 扁平化
+type Flatten1<T> = T extends any[] ? T[number] : T;
+
+const tmp_arr1 = ["xxx",213]
+type F1 = Flatten1< typeof tmp_arr1 > // string | number
+
+// 这个也跟 infer 延迟 推断 有关
+type GetReturnType<Type> = Type extends (...args: never[]) => infer Return
+    ? Return
+    : never;
+const tmp_fn1 = () => "hello world!"
+type F2 = GetReturnType<typeof tmp_fn1> // string
+type F3 = ReturnType<typeof tmp_fn1> // string
+// type ReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : any; // 官方的写法
+
+type F4 = ReturnType<typeof createLabel> // IdLabel | NameLabe
+
+// 分配条件类型
+type ToArray1<Type> = Type extends any ? Type[] : never;
+type T1 = ToArray1< string | number > // string[] | number[]
+
+// 通常，分配性是期望的行为。extends为避免这种行为，您可以用方括号将关键字的每一侧括起来。
+// [] 包裹
+type ToArray2<Type> = [Type] extends [any] ? Type[] : never;
+type T2 = ToArray2< string | number > // (string | number)[]
---
+++ main/learn-ts/types-from-types/indexed-access-types.ts
@@ -0,0 +1,33 @@
+// 索引访问类型
+
+type User1 = {
+    id: bigint;
+    name: string;
+    age: number;
+}
+
+type UserId = User1["id"] // bigint
+
+type T1 = keyof User1; // keyof User 也是一种类型 表示的是 User 的索引类型
+type T2 = User1[ keyof User1 ] // 这种会直接列出 所有索引类型的值类型
+
+type T3 = "id" | "name"
+
+type T4 = User1[T3] // 返回值类型
+type T5 = User1[T1] // 返回值类型
+
+// 获取数组对象的类型进行推断
+const user_arr = [
+    { id: 1n, name: "Alice", age: 15 },
+    { id: 2n, name: "Bob", age: 23 },
+    { id: 3n, name: "Eve", age: 38 },
+];
+
+type User2 = typeof user_arr[number]; // 获取值类型，且上下文推断
+
+// 请牢记 索引使用的是类型 而非使用变量
+const key = "age"
+// type UserAge = User1[key] // 类型[变量x] error
+type AgeKey = typeof key; // 文字类型
+type UserAge1 = User1[AgeKey] // 传入的是类型 而非值
+type UserAge2 = User1[typeof key] // 对 key 做上下文推断获取类型 string
