69477a73bad7f6055cbc15d1f5fa813ab18b9c43
Lai <1626282810@qq.com>
2022-09-02 08:09:07 +0800

    1. learn ts

---
+++ main/learn-ts/types-from-types/mapped-types.ts
@@ -0,0 +1,69 @@
+// 映射类型
+
+// 当您不想重复自己时，有时一种类型需要基于另一种类型。
+// 映射类型建立在索引签名的语法之上，用于声明未提前声明的属性类型
+
+type IndexType = {
+    [index: string]: any
+}
+const v1: IndexType = {
+    name: "hello world!"
+}
+
+type MappedTypeBoolean1<Type> = {
+    [ Property in keyof Type ]: boolean
+}
+type MappedTypeBoolean2<Type> = {
+    -readonly [ Property in keyof Type ]: boolean
+}
+type MappedTypeBoolean3<Type> = {
+    -readonly [ Property in keyof Type ]-?: boolean
+}
+
+type User = {
+    readonly id: 1n;
+    name: string;
+
+    age?: number;
+
+    goToWork: () => void;
+};
+
+
+type UserBoolean1 = MappedTypeBoolean1<User>
+type UserBoolean2 = MappedTypeBoolean2<User> // readonly 去除
+type UserBoolean3 = MappedTypeBoolean3<User> // readonly、可选 去除
+
+
+// as 重命名
+// 可以利用模板文字类型等功能从以前的属性名称中创建新的属性名称
+type Getters<Type> = {
+    [Property in keyof Type as `get${Capitalize<string & Property>}`]: () => Type[Property]
+};
+
+type GettersUser = Getters<User>
+
+// as 也可以过滤
+type RemoveIdField<Type> = {
+    [Property in keyof Type as Exclude<Property, "id">]: Type[Property]
+};
+type RemoveIdFieldUUser = RemoveIdField<User> // id 被去除了
+
+// 映射联合
+type EventConfig<Event extends {kind: string}> = {
+    [ E in Event as E["kind"] ]: (event: E) => void;
+}
+
+type SquareEvent = { kind: "square", x: number, y: number }
+type CircleEvent = { kind: "circle", radius: number }
+type Config = EventConfig< SquareEvent | CircleEvent >
+
+// 条件转换
+type ExtractPII<Type> = {
+    [Property in keyof Type]: Type[Property] extends {pii: true} ? true : false
+}
+type DBFields = {
+    id: { format: "incrementing" },
+    name: {type: string, pii: true}
+}
+type ObjectsNeedingGDPRDeletion = ExtractPII<DBFields>
---
+++ main/learn-ts/types-from-types/template-literal-types.ts
@@ -0,0 +1,82 @@
+// 模板文字
+
+// 模板文字类型建立在字符串文字类型之上，并且能够通过联合扩展成许多字符串
+import * as stream from "stream";
+import {Logger} from "sass";
+
+type World = "world"
+type HelloWorld = `hello ${World}`
+
+type EmailLocaleIDs = "welcome_email" | "email_heading";
+type FooterLocaleIDs = "footer_title" | "footer_sendoff";
+
+type AllLocaleIDs = `${EmailLocaleIDs | FooterLocaleIDs}_id`; // 四个字符串文字类型 且后缀 + _id
+type Lang = "en" | "ja" | "pt";
+type LocaleMessageIDs = `${Lang}_${AllLocaleIDs}`; // 4 * 3 = 12 类型
+
+type User = {
+    id: bigint;
+    name: string
+}
+// 这种 可以确定实现的 on 里的 事件名 正确类型，但是 callback 的参数类型就存在瑕疵
+type PropEventSource1<Type> = {
+    on(eventName: `${string & keyof Type}Changed`, callback: (newValue: Type[keyof Type]) => void): void;
+};
+
+// 缺陷
+type UserEvent1 = PropEventSource1<User>
+type UserChange1 = User & UserEvent1
+const u1: UserChange1 = {
+    id: 1n,
+    name: "hello world",
+
+    on(){}
+} as UserChange1
+u1.on("idChanged", (newValue) => {
+    // 类型是 string | bigint
+    if (typeof newValue === "bigint")
+        return
+
+    newValue.trim() // 才合理
+})
+
+
+
+// 这种 可以确定实现的 on 里的 事件名 callback的参数类型 都可以拿到正确的类型
+type PropEventSource2<Type> = {
+    on<Key extends string & keyof Type>(eventName: `${Key}Changed`, callback: (newValue: Type[Key]) => void): void;
+};
+
+type UserEvent2 = PropEventSource2<User>
+type UserChange2 = User & UserEvent2
+
+const u2: UserChange2 = {
+    id: 1n,
+    name: "hello world",
+
+    on(){}
+} as UserChange2
+
+// 拿到的都是正确类型
+u2.on('idChanged', (newValue) => {
+    console.log('bigint: ',newValue) // bigint
+})
+u2.on('nameChanged', (newValue) => {
+    console.log('string: ',newValue) // string
+})
+
+
+
+// 内在字符串操作类型
+// 它在 mapped-types 也经常使用到
+type Greeting = "Hello, world"
+type UppercaseGreeting = Uppercase<Greeting> // 转大写
+
+type IDUppercase<Str extends string> = `ID_${Uppercase<Str>}`
+type IDUppercaseMyApp = IDUppercase<"my_app"> // ID_MY_APP
+
+// Lowercase 小写转化
+// Capitalize 将字符串中的第一个字符转换为等效的大写字母。
+// Uncapitalize 将字符串中的第一个字符转换为等效的小写字母。
+type IDCapitalize<Str extends string> = `ID_${Capitalize<Str>}`
+type IDCapitalizeMyApp = IDCapitalize<"my_app"> // ID_My_app
