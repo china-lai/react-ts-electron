ef8e3979256d0dd2c0b21fc4aafb13367a9b94e3
Lai <1626282810@qq.com>
2022-09-07 07:56:42 +0800

    1. learn ts

--- main/learn-ts/classes/classes_basic.ts
+++ main/learn-ts/classes/classes_basic.ts
@@ -88,8 +88,41 @@ class User3 extends User1 {
 const u3 = new User3(2n, "LaiWenJie");
 console.log(u3.toString())
 
-// 初始化顺序
+// 可见性
 
+// default 素有类可见
+// public 所有类可见
+// protected 仅子类可见
+// private 仅当前类可见
+// 牢记 良好的规范总是从
+// public 修饰 方法
+// protected 修饰内置通用方法
+// private 修饰字段 修饰内置方法
+// private 修饰内置方法
+class User4 {
+    private _name: string;
+
+    constructor(name: string) {
+        this._name = name;
+    }
+
+    // 类似 java 的封装 getter 和 setter
+    set name(name: string) {
+        this._name = name
+    }
+    get name(){
+        return this._name
+    }
+
+}
+
+const u4 = new User4("Lai");
+// u4 不能直接访问 name 需要修改需要调用 set_name 需要取值需要返回 get_name
+u4.name = "hello world!"
+console.log(u4)
+// getter setter 优势就是在于包裹一层
+// 1. 可以在包裹的函数内做通用的事情，如 统一的 脱敏 加解密
+// 2. 如果类后续需要改实现（同时保持兼容性），比如 _first_name _last_name 替换 实现，可以直接替换，而不影响
 
 
 export {}
